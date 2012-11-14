8e450fd4838d0686099046d58f2a0e63aeb0fa52
Tim Filler <info@elricco.net>
2012-11-14 17:55:59 +0100

    v1 of Output

--- module_landingblock_output.php
+++ module_landingblock_output.php
@@ -3,7 +3,7 @@
 Module-Version: 0.1
 Author:         Tim Filler (elricco)
 Specials:       For landing Blocks on startpage or landingpages
-                defined on grid system (usual width: 280px)
+                defined on 960gs Gridsystem (usual width: 280px)
                 For UTF-8
           
 Values:         REX_VALUE[1]  - Headline
@@ -15,28 +15,112 @@ Values:         REX_VALUE[1]  - Headline
                 REX_VALUE[5]  - Linktext
                 REX_LINK[1]   - Link (internally)
                 REX_VALUE[6]  - Link (externally)
-                REX_VALUE[7]  - Buttonclass (none, .small_button, .big_button)
+                REX_VALUE[7]  - Buttonclass (none, .small-button, .big-button)
                 REX_VALUE[8]  - Linktext
+                
+                ImageManager  - landing_pic (280 x 180px w crop)
 */
 
   //Variables
   $output   = '';
-  $bclass1  = '';
-  $bclass2  = '';
+  //if you change the pics dimensions, change these also
+  $imgw = 280;
+  $imgh = 180;
+  
   
   //Functions
+  if(!function_exists("generateLink"))
+  {
+    function generateLink($intlink,$extlink,$linkclass,$linktext)
+    {
+      //Set Buttonclass
+      switch ($linkclass)
+      {
+        case 1:
+          $class = 'small-button';
+          break;
+        case 2:
+          $class = 'big-button';
+          break;
+      }
+      if ($extlink != '')
+      {
+        $bclass = ' class="'.$class.' external"';
+      }
+      else
+      {
+        $bclass = ' class="'.$class.'"';
+      }
+      //Internal Linkgeneration
+      if($intlink != '')
+      {
+        $linkart  = OOArticle::getArticleById($linkid);
+        $linkname = $linkart->getName();
+        if($linktext != '')
+        {
+          $link = '<a'.$bclass.' href="'.rex_getUrl($intlink).'" title="'.$linktext.'">'.$linktext.'</a>'."\n";
+        }
+        else
+        {
+          $link = '<a'.$bclass.' href="'.rex_getUrl($intlink).'" title="'.$linkname.'">'.$linkname.'</a>'."\n";        
+        }
+      }
+      //External Linkgeneration
+      else
+      {
+        if($linktext != '')
+        {
+          $link = '<a'.$bclass.' href="'.$extlink.'" title="'.$linktext.'">'.$linktext.'</a>'."\n";
+        }
+        else
+        {
+          $link = '<a'.$bclass.' href="'.$extlink.'" title="Mehr erfahren &gt;">Mehr erfahren &gt;</a>'."\n";        
+        }      
+      }
+      return $link;
+    }
+  }
   
   //Generate Output
   if("REX_VALUE[1]" != '')
   {
     $output  = '<div class="landingblock grid_6">'."\n";
-    
-    $output .= '  <div class="linkblock">'."\n";
-    $output .= generateLink("REX_LINK_ID[1]","REX_VALUE[3]","REX_VALUE[4]","REX_VALUE[5]");
-    $output .= generateLink("REX_LINK_ID[2]","REX_VALUE[6]","REX_VALUE[7]","REX_VALUE[8]");    
+    $output .= '  <div class="blockhead">'."\n";
+    $output .= '    <h2>REX_VALUE[1]</h2>'."\n";
     $output .= '  </div>'."\n";
+    if("REX_MEDIA[1]" != '')
+    {
+      $mediafile = OOMedia::getMediaByFileName("REX_VALUE[1]");
+      //Just in case we need more infos
+      $mediapath = $mediafile->getFullPath();
+      $mediawidth = $mediafile->getWidth();
+      $mediaheight = $mediafile->getHeight();
+      $medianame = $mediafile->getTitle();
+      
+      $output .= '  <div class="landingpic">'."\n";
+      $output .= '    <img src="index.php?rex_img_type=landing_pic&amp;rex_img_file=REX_MEDIA[1]" alt="'.$medianame.'" width="'.$imgw.'" height="'.$imgh.'" />'."\n";
+      $output .= '  </div>'."\n";
+    }
+    
+    
+    
+    $output .= '  ';
+    if("REX_LINK[1]" != '' || "REX_VALUE[3]" != '' || "REX_LINK[2]" != '' || "REX_VALUE[6]" != '')
+    {
+      $output .= '  <div class="linkblock">'."\n";
+      $output .= generateLink("REX_LINK_ID[1]","REX_VALUE[3]","REX_VALUE[4]","REX_VALUE[5]");
+      $output .= generateLink("REX_LINK_ID[2]","REX_VALUE[6]","REX_VALUE[7]","REX_VALUE[8]");    
+      $output .= '  </div>'."\n";
+    }
     $output .= '</div>'."\n";
-  }  
+  }
+  else
+  {
+    if($REX['REDAXO'])
+    {
+      echo 'Bitte mindestens eine &Uuml;berschrift eingeben!';
+    }
+  }
   
   
   
